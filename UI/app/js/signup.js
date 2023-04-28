179eb577e6c7444d08997053fd2858826b64a659
Vanessa Zanotti <zanottivanessa11@gmail.com>
2023-04-27 21:49:41 -0300

    FEAT - Criação de app para hamburgueria

--- UI/app/js/signup.js
+++ UI/app/js/signup.js
@@ -1,6 +1,6 @@
 // CONSUME SIGNUP ENDPOINT
 const signupBtn = document.getElementById('signupBtn');
-const name = document.getElementById('username');
+const nameUser = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
@@ -18,37 +18,56 @@ const password2Err = document.querySelector('div#password2Err');
 // Adapted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
 function isValidPassword(value) {
   if (!/[a-z]/.test(value)) {
-    return 'Your password must contain at least one lowercase letter';
-  } if (!/[A-Z]/.test(value)) {
-    return 'Your password must contain at least one uppercase letter';
-  } if (!/\d/.test(value)) {
-    return 'Your password must contain at least one number';
-  } if (!/[@$!%*?&]/.test(value)) {
-    return 'Your password must contain at least one of these special characters: @, $, !, %, *, ?, &';
-  } if (value.length < 6) {
-    return 'Your password must be composed of at least 6 characters';
+    return 'Sua senha deve conter pelo menos uma letra minúscula';
+  }
+  if (!/[A-Z]/.test(value)) {
+    return 'Sua senha deve conter pelo menos uma letra maiúscula';
+  }
+  if (!/\d/.test(value)) {
+    return 'Sua senha deve conter pelo menos um número';
+  }
+  if (!/[@$!%*?&]/.test(value)) {
+    return 'Sua senha deve conter pelo menos um destes caracteres especiais: @, $, !, %, *, ?, &';
+  }
+  if (value.length < 6) {
+    return 'Sua senha deve ser composta por pelo menos 6 caracteres';
   }
   return 'true';
 }
 
-name.onchange = () => {
+nameUser.onchange = () => {
   password2Err.innerHTML = '';
 };
 email.onchange = () => {
-  emailErr.innerHTML = /\S+@\S+\.\S+/.test(email.value) ? '' : 'Please enter a valid email';
+  emailErr.innerHTML = /\S+@\S+\.\S+/.test(email.value)
+    ? ''
+    : 'Por favor, insira um e-mail válido';
   usernameErr.innerHTML = '';
   password2Err.innerHTML = '';
 };
 password.onchange = () => {
-  passwordErr.innerHTML = isValidPassword(password.value) === 'true' ? '' : isValidPassword(password.value);
-  password2Err.innerHTML = password2.value === password.value ? '' : 'Passwords don\'t match';
+  passwordErr.innerHTML =
+    isValidPassword(password.value) === 'true'
+      ? ''
+      : isValidPassword(password.value);
+  password2Err.innerHTML =
+    password2.value === password.value
+      ? ''
+      : 'As senhas são diferentes, verifique!';
 };
 password2.oninput = () => {
-  password2Err.innerHTML = password2.value === password.value ? '' : 'Passwords don\'t match';
+  password2Err.innerHTML =
+    password2.value === password.value
+      ? ''
+      : 'As senhas são diferentes, verifique!';
 };
 
 signupBtn.onmouseover = () => {
-  if (emailErr.innerHTML !== '' || passwordErr.innerHTML !== '' || password2Err.innerHTML !== '') {
+  if (
+    emailErr.innerHTML !== '' ||
+    passwordErr.innerHTML !== '' ||
+    password2Err.innerHTML !== ''
+  ) {
     signupBtn.style.opacity = 0.6;
   } else {
     signupBtn.style.opacity = 1;
@@ -60,12 +79,15 @@ const localhost = 'http://localhost:9999/api/v1';
 // UNCOMMENT BELOW AND USE IN REQ FOR PRODUCTION
 // const herokuhost = 'https://fast-food-fast-bobsar0.herokuapp.com/api/v1/';
 
-
 signupBtn.onclick = () => {
-  if (emailErr.innerHTML !== '' || passwordErr.innerHTML !== '' || password2Err.innerHTML !== '') {
-    usernameErr.innerHTML = 'Please correct the errors in red below';
+  if (
+    emailErr.innerHTML !== '' ||
+    passwordErr.innerHTML !== '' ||
+    password2Err.innerHTML !== ''
+  ) {
+    usernameErr.innerHTML = 'Por favor, corrija os erros abaixo:';
   } else {
-    const username = name.value;
+    const username = nameUser.value;
     usernameErr.innerHTML = '';
     const req = new Request(`${localhost}/auth/signup`, {
       method: 'POST',
@@ -73,37 +95,47 @@ signupBtn.onclick = () => {
         'Content-Type': 'application/json',
       },
       // mode: 'no-cors',
-      body: JSON.stringify({ username, email: email.value, password: password.value }),
+      body: JSON.stringify({
+        username,
+        email: email.value,
+        password: password.value,
+      }),
     });
-    fetch(req).then(resp => resp.json().then((res) => {
-      if (res.status === 'fail') {
-        password2Err.innerHTML = res.message;
-      }
-      if (res.status === 'success' && res.token) {
-        localStorage.setItem('token', res.token);
-        localStorage.setItem('username', res.user.username);
-        localStorage.setItem('address', res.user.address);
-        localStorage.setItem('phone', res.user.phone);
-        localStorage.setItem('id', res.user.userid);
+    fetch(req)
+      .then((resp) =>
+        resp
+          .json()
+          .then((res) => {
+            if (res.status === 'fail') {
+              password2Err.innerHTML = res.message;
+            }
+            if (res.status === 'success' && res.token) {
+              localStorage.setItem('token', res.token);
+              localStorage.setItem('username', res.user.username);
+              localStorage.setItem('address', res.user.address);
+              localStorage.setItem('phone', res.user.phone);
+              localStorage.setItem('id', res.user.userid);
 
-        password2Err.innerHTML = `<span style='color: greenyellow'>${res.message}</span>`;
+              password2Err.innerHTML = `<span style='color: greenyellow'>${res.message}</span>`;
 
-        if (res.user.role === 'admin') {
-          setTimeout(() => {
-            localStorage.setItem(res.user.username, 'an');
-            window.location.href = 'admin';
-          }, 100);
-          return;
-        }
-        setTimeout(() => {
-          window.location.href = 'userMenu';
-        }, 100);
-      }
-    }).catch((err) => {
-      password2Err.innerHTML = err.message;
-    }))
-      .catch(((fetchErr) => {
+              if (res.user.role === 'admin') {
+                setTimeout(() => {
+                  localStorage.setItem(res.user.username, 'an');
+                  window.location.href = 'admin';
+                }, 100);
+                return;
+              }
+              setTimeout(() => {
+                window.location.href = 'userMenu';
+              }, 100);
+            }
+          })
+          .catch((err) => {
+            password2Err.innerHTML = err.message;
+          }),
+      )
+      .catch((fetchErr) => {
         usernameErr.innerHTML = `Error: ${fetchErr}... Offline?`;
-      }));
+      });
   }
 };
--- UI/js/cart.js
+++ UI/js/cart.js
@@ -2,7 +2,6 @@ const localhost = 'http://localhost:9999/api/v1';
 // UNCOMMENT BELOW AND USE IN REQ FOR PRODUCTION
 // const herokuhost = 'https://fast-food-fast-bobsar0.herokuapp.com/api/v1/';
 
-
 /** ADD TO CART IMPLEMENTATION*** */
 
 const cartTable = document.getElementById('cartTable');
@@ -24,7 +23,8 @@ let trArr = [];
 
 // Helper function that appends row containing data to table
 function appendtoTable(cellArr, tr, tableName) {
-  cellArr.forEach((cell) => { // append each cell to td then to tr
+  cellArr.forEach((cell) => {
+    // append each cell to td then to tr
     const td = document.createElement('TD'); // create table data
     td.appendChild(cell);
     tr.appendChild(td);
@@ -53,15 +53,17 @@ let cartArray = [];
 // CartQuantity
 let totalQty = 0;
 
-
 setTimeout(() => {
   const cartBtns = document.getElementsByClassName('cartBtn');
   // Listen for a click event on each 'Add to Cart' button and append order info to shopping cart
   [...cartBtns].forEach((cartBtn) => {
     cartBtn.addEventListener('click', () => {
-      if (!document.getElementById('menuWelcome').textContent.includes('Welcome ')) {
-      // open modal asking user to sign up
-        msg.innerHTML = ('Please <a href="/signup"><b>signup</b></a> or <a href="/login"><b>login</b></a> to continue with your order');
+      if (
+        !document.getElementById('menuWelcome').textContent.includes('Welcome ')
+      ) {
+        // open modal asking user to sign up
+        msg.innerHTML =
+          'Por favor <a href="/signup"><b>cadastre-se</b></a> ou <a href="/login"><b>login</b></a> para continuar com seu pedido!';
         displayModal(generalModal, span1);
         return;
       }
@@ -70,7 +72,9 @@ setTimeout(() => {
 
       const name = document.getElementById(`item${uniqueId}`).innerHTML;
       const img = document.getElementById(`img${uniqueId}`);
-      let quantity = Number(document.querySelector(`select#selectQty${uniqueId}`).value);
+      let quantity = Number(
+        document.querySelector(`select#selectQty${uniqueId}`).value,
+      );
       let price = document.getElementById(`price${uniqueId}`).innerHTML;
       price = quantity * Number(price.slice(4));
 
@@ -80,7 +84,7 @@ setTimeout(() => {
 
       orders.forEach((order) => {
         if (order.name === name) {
-        // remove everything associated with similar order of which updated one will be re-added
+          // remove everything associated with similar order of which updated one will be re-added
           quantity += order.quantity;
           price += order.price;
           totalPrice -= order.price;
@@ -90,7 +94,7 @@ setTimeout(() => {
 
           trArr.forEach((tr) => {
             if (tr.textContent.includes(order.name)) {
-            // remove row from trArr
+              // remove row from trArr
               trArr.splice(trArr.indexOf(tr), 1);
               // remove row from cartTable
               cartTable.removeChild(tr);
@@ -121,7 +125,7 @@ setTimeout(() => {
       localStorage.setItem('totalPrice', JSON.stringify(totalPrice));
 
       // Open a modal
-      msg.innerHTML = (`<b>${qty}x ${name}</b> successfully added to cart`);
+      msg.innerHTML = `<b>${qty}x ${name}</b> Adicionado ao carrinho com sucesso!`;
       displayModal(generalModal, span1);
 
       // Create contents for the table data cells in each row
@@ -169,7 +173,7 @@ setTimeout(() => {
         // remove row from cartCellArr
         cartCellArr.forEach((cell) => {
           if (cell[cell.length - 1].id === cancelBtn.id) {
-          // Delete cell
+            // Delete cell
             cartCellArr.splice(cartCellArr.indexOf(cell), 1);
             // remove row from order and cartArray
             orders.forEach((order) => {
@@ -235,11 +239,11 @@ const orderHistory = document.getElementById('tableHistory');
 // CHECKOUT BUTTON
 checkoutBtn.onclick = () => {
   if (!address.value || address.value === 'null') {
-    cartErr.innerHTML = 'Please fill in your delivery address';
+    cartErr.innerHTML = 'Por favor, preencha seu endereço de entrega';
     return;
   }
   if (!phone.value || phone.value === 'null') {
-    cartErr.innerHTML = 'Please fill in your phone number';
+    cartErr.innerHTML = 'Por favor, preencha seu endereço de entrega';
     return;
   }
   cartErr.innerHTML = '';
@@ -258,76 +262,86 @@ checkoutBtn.onclick = () => {
         'Content-Type': 'application/json',
         'x-access-token': localStorage.token,
       },
-      body: JSON.stringify({ cartArray, address: address.value, phone: phone.value }),
+      body: JSON.stringify({
+        cartArray,
+        address: address.value,
+        phone: phone.value,
+      }),
     });
-    fetch(req).then((resp) => {
-      resp.json().then((res) => {
-        if (res.error) {
-          msg.innerHTML = `<p style="color: red">Error: ${res.error.message || res.error}</p>`;
-          displayModal(generalModal, span1);
-        } else if (res.status === 'fail') {
-
-          cartErr.innerHTML = `<p>${res.message}</p>`;
-        } else if (res.status === 'success') {
-          let i = 0;
-          const { orderid, userid, food } = res.order;
-          msg.innerHTML = `<span style="color: green"><b>${res.message}!</b></span>
-          <h4 style="text-decoration: underline"> YOUR ORDER DETAILS: </h4>
+    fetch(req)
+      .then((resp) => {
+        resp
+          .json()
+          .then((res) => {
+            if (res.error) {
+              msg.innerHTML = `<p style="color: red">Error: ${
+                res.error.message || res.error
+              }</p>`;
+              displayModal(generalModal, span1);
+            } else if (res.status === 'fail') {
+              cartErr.innerHTML = `<p>${res.message}</p>`;
+            } else if (res.status === 'success') {
+              let i = 0;
+              const { orderid, userid, food } = res.order;
+              msg.innerHTML = `<span style="color: green"><b>${res.message}!</b></span>
+          <h4 style="text-decoration: underline"> DETALHES DO SEU PEDIDO: </h4>
           <p><span style="color: blue">Order ID</span>: <b>#${userid}FFF${orderid}</b></p>`;
 
-          food.forEach((item) => {
-            const { name, quantity } = item;
-            i += 1;
-            const p = document.createElement('P');
-            p.innerHTML = `<span style="color: blue">Food${i}</span>: <b>${quantity}x ${name}</b>`;
-            msg.appendChild(p);
-          });
+              food.forEach((item) => {
+                const { name, quantity } = item;
+                i += 1;
+                const p = document.createElement('P');
+                p.innerHTML = `<span style="color: blue">Food${i}</span>: <b>${quantity}x ${name}</b>`;
+                msg.appendChild(p);
+              });
 
-          const div = document.createElement('DIV');
-          div.innerHTML = `  
+              const div = document.createElement('DIV');
+              div.innerHTML = `  
           <p><span style="color: blue">Total Quantity</span>: <b>${res.order.quantity}</b></p>
           <p><span style="color: blue">Price</span>: <b>&#x20a6; ${res.order.price}.00</b></p>
-          <br>We will contact you shortly at <b>${phone.value}</b> or <b>${localStorage.email}</b> with further details.
-          <h6 style="color: red"><i>Please note your Order ID for any correspondence related to this order.</i></h6>`;
+          <br>Nós vamos entrar em contato com você no telefone <b>${phone.value}</b> or <b>${localStorage.email}</b> para te passar mais detalhes.
+          <h6 style="color: red"><i> Por favor, verifique o id correspondente ao seu pedido.</i></h6>`;
 
-          msg.appendChild(div);
-          displayModal(generalModal, span1);
+              msg.appendChild(div);
+              displayModal(generalModal, span1);
 
-          totalPrice = 0;
-          total.innerHTML = totalPrice.toFixed(2);
-          // Remove rows from cart table
-          trArr.forEach(((tr) => {
-            cartTable.removeChild(tr); // remove row from table
-          }));
-          // Clear trArr
-          trArr = [];
+              totalPrice = 0;
+              total.innerHTML = totalPrice.toFixed(2);
+              // Remove rows from cart table
+              trArr.forEach((tr) => {
+                cartTable.removeChild(tr); // remove row from table
+              });
+              // Clear trArr
+              trArr = [];
 
-          // Record order in order history table
-          cartCellArr.forEach((cells) => {
-            // Add date as first element in cell
-            const date = document.createTextNode(`${new Date()}`);
-            cells.unshift(date);
-            cells.pop();
-            const trHist = document.createElement('TR');
-            appendtoTable(cells, trHist, orderHistory);
+              // Record order in order history table
+              cartCellArr.forEach((cells) => {
+                // Add date as first element in cell
+                const date = document.createTextNode(`${new Date()}`);
+                cells.unshift(date);
+                cells.pop();
+                const trHist = document.createElement('TR');
+                appendtoTable(cells, trHist, orderHistory);
+              });
+              // Reset data
+              cartCellArr = [];
+              orders = [];
+              cartArray.length = 0;
+              totalQty = 0;
+              totalItems.innerHTML = 0;
+              localStorage.removeItem(orders);
+              localStorage.removeItem(cartArray);
+              // Style button
+              checkoutBtn.style.backgroundColor = '#212121';
+              checkoutBtn.style.cursor = 'not-allowed';
+              checkoutBtn.style.color = 'goldenrod';
+              checkoutBtn.style.opacity = 0.6;
+            }
+          })
+          .catch((err) => {
+            console.error('err in placing order:', err);
           });
-          // Reset data
-          cartCellArr = [];
-          orders = [];
-          cartArray.length = 0;
-          totalQty = 0;
-          totalItems.innerHTML = 0;
-          localStorage.removeItem(orders);
-          localStorage.removeItem(cartArray);
-          // Style button
-          checkoutBtn.style.backgroundColor = '#212121';
-          checkoutBtn.style.cursor = 'not-allowed';
-          checkoutBtn.style.color = 'goldenrod';
-          checkoutBtn.style.opacity = 0.6;
-        }
-      }).catch((err) => {
-        console.error('err in placing order:', err);
-      });
-    }).catch(fetchErr => console.error('fetcherr:', fetchErr));
+      })
+      .catch((fetchErr) => console.error('fetcherr:', fetchErr));
   }
 };
